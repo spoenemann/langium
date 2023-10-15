7cc794318278ee47a9e2d2423547e74ecd335e98
pluralia <irina.artemeva@typefox.io>
2023-10-15 09:36:09 +0200

    Add main function

--- examples/arithmetics/llvm/basicMath.ll
+++ examples/arithmetics/llvm/basicMath.ll
@@ -2,12 +2,20 @@
 source_filename = "example/example.calc"
 target triple = "arm64-apple-macosx14.0.0"
 
-@str = private unnamed_addr constant [11 x i8] c"HelloWorld\00", align 1
-@str.1 = private unnamed_addr constant [11 x i8] c"HelloWorld\00", align 1
+@float_modifier = private unnamed_addr constant [4 x i8] c"%f\0A\00", align 1
 
 declare i32 @printf(i8*, ...)
 
 define i64 @main() {
-  %qwe = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([11 x i8], [11 x i8]* @str, i32 0, i32 0))
-  ret i64 5
+entry:
+  %a = alloca double, align 8
+  store double 5.000000e+00, double* %a, align 8
+  %b = alloca double, align 8
+  store double 3.000000e+00, double* %b, align 8
+  %0 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @float_modifier, i32 0, i32 0), double 4.220000e+01)
+  %b1 = load double, double* %b, align 8
+  %a2 = load double, double* %a, align 8
+  %sum_b_a = fadd double %b1, %a2
+  %1 = call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @float_modifier, i32 0, i32 0), double %sum_b_a)
+  ret i64 0
 }
--- examples/arithmetics/src/cli/llvm-ir-generator.ts
+++ examples/arithmetics/src/cli/llvm-ir-generator.ts
@@ -5,14 +5,16 @@
  ******************************************************************************/
 
 import { CompositeGeneratorNode, toString } from 'langium';
-import { type Definition, type Evaluation, isDefinition, type Module, type Statement } from '../language-server/generated/ast.js';
-import { type Expression, isNumberLiteral } from '../language-server/generated/ast.js';
+import { type Definition, type Evaluation, type Module, type Statement, isEvaluation, type Expression, isBinaryExpression, isFunctionCall } from '../language-server/generated/ast.js';
+import { isNumberLiteral } from '../language-server/generated/ast.js';
 import llvm from 'llvm-bindings';
 
 type LLVMData = {
     context: llvm.LLVMContext,
     module: llvm.Module,
     builder: llvm.IRBuilder,
+    globalVars: Map<string, llvm.Constant>,
+    mainVars: Map<string, llvm.AllocaInst>
 }
 
 const CustomLLVMConfig = {
@@ -26,7 +28,9 @@ export function generateLLVMIR(exprSet: Module, fileName: string): string {
     // module.setTargetTriple(llvm.config.LLVM_DEFAULT_TARGET_TRIPLE);
     module.setTargetTriple(CustomLLVMConfig.TARGET_TRIPLE);
     const builder = new llvm.IRBuilder(context);
-    const llvmData = { context, module, builder };
+    const globalVars = new Map<string, llvm.Constant>();
+    const mainVars = new Map<string, llvm.AllocaInst>();
+    const llvmData = { context, module, builder, globalVars, mainVars };
 
     setupExternFunctions(llvmData);
 
@@ -37,19 +41,38 @@ export function generateLLVMIR(exprSet: Module, fileName: string): string {
 }
 
 function setupExternFunctions(llvmData: LLVMData) {
-    const bytePtrTy: llvm.PointerType[] = [llvmData.builder.getInt8PtrTy()];
+    const float_modifier_name = 'float_modifier';
+    const float_modifier = llvmData.builder.CreateGlobalStringPtr('%f\n', float_modifier_name, 0, llvmData.module);
+    llvmData.globalVars.set(float_modifier_name, float_modifier);
 
+    const bytePtrTy: llvm.PointerType[] = [llvmData.builder.getInt8PtrTy()];
     llvmData.module.getOrInsertFunction('printf', llvm.FunctionType.get(
         /* return type */ llvmData.builder.getInt32Ty(),
-        /* foramt arg */ bytePtrTy,
-        /* vararg */ true
+        /* foramt arg */  bytePtrTy,
+        /* vararg */      true
     ));
 }
 
 function generateLLVMIRinternal(statements: Statement[], llvmData: LLVMData): string {
     const { module } = llvmData;
 
-    statements.forEach(stmt => generateStatement(stmt, llvmData));
+    const functions = [];
+    const variables = [];
+    const evaluations = [];
+    for (const stmt of statements) {
+        if (isEvaluation(stmt)) {
+            evaluations.push(stmt);
+        } else {
+            if (stmt.args.length === 0) {
+                variables.push(stmt);
+            } else {
+                functions.push(stmt);
+            }
+        }
+    }
+
+    // functions.forEach(func => generateFunction(func, llvmData));
+    generateMainFunction(variables, evaluations, llvmData);
 
     if (llvm.verifyModule(module)) {
         return 'Verifying module failed';
@@ -57,52 +80,95 @@ function generateLLVMIRinternal(statements: Statement[], llvmData: LLVMData): st
     return module.print();
 }
 
-function generateStatement(stmt: Statement, llvmData: LLVMData): void {
-    if (isDefinition(stmt)) {
-        generateDefinition(stmt, llvmData);
-    } else {
-        generateEvaluation(stmt, llvmData);
-    }
-}
-
-function generateDefinition(def: Definition, llvmData: LLVMData): void {
+function generateMainFunction(variables: Definition[], evaluations: Evaluation[], llvmData: LLVMData): void {
     const { context, module, builder } = llvmData;
 
     const returnType = builder.getInt64Ty();
-    const paramTypes: llvm.Type[] = def.args.map(_ => builder.getInt64Ty());
-    const functionType = llvm.FunctionType.get(returnType, paramTypes, false);
-    const func = llvm.Function.Create(functionType, llvm.Function.LinkageTypes.ExternalLinkage, def.name, module);
+    const functionType = llvm.FunctionType.get(returnType, [], false);
+    const mainFunc = llvm.Function.Create(functionType, llvm.Function.LinkageTypes.ExternalLinkage, 'main', module);
 
-    const entryBB = llvm.BasicBlock.Create(context, 'main', func);
+    const entryBB = llvm.BasicBlock.Create(context, 'entry', mainFunc);
     builder.SetInsertPoint(entryBB);
 
-    const str = builder.CreateGlobalStringPtr('HelloWorld', 'str', 0, module);
-    const printfFn = llvmData.module.getFunction('printf');
-    if (printfFn) {
-        llvmData.builder.CreateCall(printfFn, [str], 'qwe');
-    }
+    variables.forEach(v => generateVariables(v, llvmData));
+    evaluations.forEach(e => generateEvaluation(e.expression, llvmData));
 
-    const namedValues = new Map<string, llvm.Argument>();
-    for (let i = 0; i < def.args.length; i++) {
-        namedValues.set(def.args[i].name, func.getArg(i));
-    }
-    const result = generateExpression(def.expr, llvmData, namedValues);
-    builder.CreateRet(result);
+    builder.CreateRet(llvmData.builder.getInt64(0));
 
-    if (llvm.verifyFunction(func)) {
+    if (llvm.verifyFunction(mainFunc)) {
         throw new Error('Error: LLVM IR generation: function generatation failed!');
     }
 }
 
-function generateExpression(expr: Expression, llvmData: LLVMData, namedValues: Map<string, llvm.Argument>): llvm.Value {
-    if (isNumberLiteral(expr)) {
-        return llvmData.builder.getInt64(expr.value);
-    } else {
-        namedValues;
+function generateVariables(variable: Definition, {builder, mainVars}: LLVMData): void {
+    if (isNumberLiteral(variable.expr)) {
+        const floatAlloca = builder.CreateAlloca(builder.getDoubleTy(), null, variable.name);
+        builder.CreateStore(llvm.ConstantFP.get(builder.getDoubleTy(), variable.expr.value), floatAlloca);
+        mainVars.set(variable.name, floatAlloca);
     }
-    throw new Error('Error: LLVM IR generation: the case must be covered by the validator!');
 }
 
-function generateEvaluation(evalAction: Evaluation, llvmData: LLVMData): void {
-    evalAction; llvmData;
+function generateEvaluation(expr: Expression, llvmData: LLVMData): void {
+    const {module, builder, globalVars, mainVars} = llvmData;
+
+    const printfFn = module.getFunction('printf');
+    if (!printfFn) return;
+    const float_modifier = globalVars.get('float_modifier')!;
+
+    if (isNumberLiteral(expr)) {
+        builder.CreateCall(printfFn, [float_modifier, llvm.ConstantFP.get(builder.getDoubleTy(), expr.value)]);
+    } else if (isBinaryExpression(expr)) {
+        if (expr.operator === '+' && isFunctionCall(expr.left) && isFunctionCall(expr.right) && expr.left.args.length === 0 && expr.right.args.length === 0) {
+            const a = expr.left.func.ref?.name;
+            const b = expr.right.func.ref?.name;
+            if (a && b) {
+                const allocaInstA = mainVars.get(a);
+                const allocaInstB = mainVars.get(b);
+                if (allocaInstA && allocaInstB) {
+                    const valueA = builder.CreateLoad(builder.getDoubleTy(), allocaInstA, a);
+                    const valueB = builder.CreateLoad(builder.getDoubleTy(), allocaInstB, b);
+                    const res = builder.CreateFAdd(valueA, valueB, `sum_${a}_${b}`);
+                    builder.CreateCall(printfFn, [float_modifier, res]);
+                }
+            }
+        }
+    }
 }
+
+// function generateFunction(def: Definition, llvmData: LLVMData): void {
+//     const { context, module, builder } = llvmData;
+
+//     const returnType = builder.getInt64Ty();
+//     const paramTypes: llvm.Type[] = def.args.map(_ => builder.getInt64Ty());
+//     const functionType = llvm.FunctionType.get(returnType, paramTypes, false);
+//     const func = llvm.Function.Create(functionType, llvm.Function.LinkageTypes.ExternalLinkage, def.name, module);
+
+//     const entryBB = llvm.BasicBlock.Create(context, 'main', func);
+//     builder.SetInsertPoint(entryBB);
+
+//     const str = builder.CreateGlobalStringPtr('HelloWorld', 'str', 0, module);
+//     const printfFn = llvmData.module.getFunction('printf');
+//     if (printfFn) {
+//         llvmData.builder.CreateCall(printfFn, [str], 'qwe');
+//     }
+
+//     const namedValues = new Map<string, llvm.Argument>();
+//     for (let i = 0; i < def.args.length; i++) {
+//         namedValues.set(def.args[i].name, func.getArg(i));
+//     }
+//     const result = generateExpression(def.expr, llvmData, namedValues);
+//     builder.CreateRet(result);
+
+//     if (llvm.verifyFunction(func)) {
+//         throw new Error('Error: LLVM IR generation: function generatation failed!');
+//     }
+// }
+
+// function generateExpression(expr: Expression, llvmData: LLVMData, namedValues: Map<string, llvm.Argument>): llvm.Value {
+//     if (isNumberLiteral(expr)) {
+//         return llvmData.builder.getInt64(expr.value);
+//     } else {
+//         namedValues;
+//     }
+//     throw new Error('Error: LLVM IR generation: the case must be covered by the validator!');
+// }
